46c27c941f110be6b817bbbde503886e01666a05
Altan Kaplan <altanka@pop-os.localdomain>
2021-09-12 16:53:23 +0300

    Implement reverse infinite scroll for chat screen

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Message from './Message.js'
 import InfiniteScroll from 'react-native-infinite-scrolling'
- 
+
 var isSenderLast = null
 
 export default function MessageList(props) {
@@ -16,11 +16,46 @@ export default function MessageList(props) {
             </View>
         )
     }
-    
 
+    const handleVieweableItemsChanged = useCallback(({ viewedItems }) => {
+        console.log(viewedItems)
+       /* setData(oldData => {
+            const newData = loadMore(oldData)
+            // We can have access to the current state without adding it
+            //  to the useCallback dependencies
+
+            // If the items didn't change, we return the old items so
+            //  an unnecessary re-render is avoided.
+            return newData;
+        });*/
+
+        // Since it has no dependencies, this function is created only once
+    }, []);
+    /*const handleVieweableItemsChanged = (currentData) => {
+        // console.log(lastViewableItem)
+        // Use viewable items in state or as intended
+        if (lastViewableItem.id == 8) {
+            var newData = []
+            var len = typeof (data) !== 'undefined' ? data.length : 0
+            console.log(len)
+            for (let index = len; index < (len + 10); index++) {
+                var isSender = Math.random() > 0.5
+                var message = {
+                    id: index,
+                    text: "Message longlonglonglonglonglonglonglonglonglonglonglonglong" + index,
+                    isSender: isSender,
+                    isSequential: isSenderLast === isSender
+                }
+                newData.push(message)
+                isSenderLast = message.isSender
+            }
+            const updatedData = typeof (data) !== 'undefined' ? data.concat(newData) : newData
+            setData(updatedData)
+        }
+    }*/
     const loadMore = () => {
         var newData = []
-        var len = typeof(data) !== 'undefined' ? data.length : 0
+        var len = typeof (data) !== 'undefined' ? data.length : 0
         for (let index = len; index < (len + 10); index++) {
             var isSender = Math.random() > 0.5
             var message = {
@@ -32,20 +67,31 @@ export default function MessageList(props) {
             newData.push(message)
             isSenderLast = message.isSender
         }
-        let updatedData = typeof(data) !== 'undefined' ? data.concat(newData) : newData
+
+        const updatedData = typeof (data) !== 'undefined' ? data.concat(newData) : newData
         setData(updatedData)
+
     }
     useEffect(() => {
         loadMore()
+        // onViewRef.current()
     }, [])
 
     return (
         <View style={styles.container}>
-            <InfiniteScroll
+            {/* <InfiniteScroll
                 renderData={renderData}
                 data={data}
                 loadMore={loadMore}
                 onReachEnd={false}
+            /> */}
+            <FlatList
+                data={data}
+                keyExtractor={(item, index) => String(index)}
+                renderItem={renderData}
+                inverted={true}
+                onEndReached={loadMore}
+                onEndReachedThreshold={50}
             />
         </View>
     );
